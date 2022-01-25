7839b05b8d9a90691604d788e6aa729a0421f7c0
kiokogit <91476609+kiokogit@users.noreply.github.com>
2022-01-25 03:09:39 +0300

    cors, mongodb url

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json({ limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({ limit:'30mb', extended:true}));
 
 //routers here
-app.use('/users',cors(), userRoutes);
-app.use('/admin',cors(), adminRoutes);
-app.use('/',cors(), forAllRoutes);
+app.use('/users', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/', forAllRoutes);
 
-const MONGO_URI =' mongodb+srv://kiokovincent:kiokokioko@firstcluster.sg8iw.mongodb.net/libSystem?'
+const MONGO_URI ='mongodb://kiokovincent:kiokokioko@firstcluster.sg8iw.mongodb.net/libSystem?'
 //database connection 1
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology:true}, (e) => e? console.log('Database Error: '+ JSON.stringify(e)):console.log('Connected to Library Database!...'));
 
